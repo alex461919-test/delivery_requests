8accdf0ed6ee5bb554293fdd5ed6f0a5230ce834
Alex Didenko <alex.461919@yandex.ru>
2022-12-07 11:13:45 +0700

    clear ref

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,32 +1,38 @@
-import React, { useCallback } from 'react';
-import { MapContainer, MapContainerProps, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useCallback, useEffect } from "react";
+import { MapContainer, MapContainerProps, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-import RouteGroup from './RouteGroup';
+import RouteGroup from "./RouteGroup";
 
-L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.7.1/dist/images/';
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.7.1/dist/images/";
 
 const Map = React.forwardRef<L.Map, MapContainerProps>((props, ref) => {
-    console.log('render Map');
+  console.log("render Map");
 
-    const onMapCreated = useCallback(
-        (map: L.Map) => {
-            if (typeof ref === 'function') ref(map);
-            if (ref && typeof ref === 'object') ref.current = map;
-        },
-        [ref],
-    );
+  const onMapCreated = useCallback(
+    (map: L.Map) => {
+      if (typeof ref === "function") ref(map);
+      if (ref && typeof ref === "object") ref.current = map;
+    },
+    [ref]
+  );
+  useEffect(
+    () => () => {
+      if (ref && typeof ref === "object") ref.current = null;
+    },
+    [ref]
+  );
 
-    return (
-        <MapContainer {...props} whenCreated={onMapCreated}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <RouteGroup />
-        </MapContainer>
-    );
+  return (
+    <MapContainer {...props} whenCreated={onMapCreated}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <RouteGroup />
+    </MapContainer>
+  );
 });
 
 export default React.memo(Map);
